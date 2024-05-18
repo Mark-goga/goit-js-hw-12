7be22bbacebaa12fb5192c8be4371af35e094ab2
Mark-goga <markgala06@gmail.com>
2024-05-18 16:44:03 +0300

    start_finish

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -2,8 +2,12 @@ import axios from "axios"
 
 const API_KEY = '43780591-6ab37bb22e1af39025cb54e89';
 axios.defaults.baseURL = 'https://pixabay.com/api/'
-export function fetchPhoto(valuePhoto) {
+export const PER_PAGE = 15;
+
+export function fetchPhoto(valuePhoto , newPage) {
     const searchParams = {
+        per_page: PER_PAGE,
+        page: newPage,
         key: API_KEY,
         q: valuePhoto,
         safesearch: true,
